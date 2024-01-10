64e546b597479c835e632b967c3a9bea90ce6212
aditi <additigupta366@gmail.com>
2024-01-10 20:10:22 +0530

    usenavigate done

--- src/component/render/About.jsx
+++ src/component/render/About.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 // import { NavLink } from 'react-router-dom';
 // import Navbar from './Navbar';
+import { useNavigate } from 'react-router-dom';
 
 
 const About = () => {
+  const navi=useNavigate();
+  const gotoContact=()=>{
+    navi("/contact");
+  }
   return(
     <>
   <h1>About page</h1>
+  <button onClick={()=>{gotoContact()}}>goto contact</button>
+  <br/>
+  <button onClick={()=>{navi(-1)}}>goback</button>
   {/* <Navbar/> */}
 </>
   )
--- src/component/render/Contact.jsx
+++ src/component/render/Contact.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 // import { NavLink } from 'react-router-dom';
 // import Navbar from './Navbar';
+import { useNavigate } from 'react-router-dom'
 
 
 const Contact = () => {
+  const navi=useNavigate();
+
+  const gotohome=()=>{
+      navi("/");
+  }
   return (
+    
     <>
           <h1>contact page</h1>
           {/* <Navbar/> */}
+          <button onClick={()=>{gotohome()}}>goto home</button>
+          <br/>
+  <button onClick={()=>{navi(-1)}}>goback</button>
+
         </>
   )
 }
