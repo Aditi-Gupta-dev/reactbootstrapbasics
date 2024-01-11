53589494448347bae137ee21512bd29a119d1a41
aditi <additigupta366@gmail.com>
2024-01-11 18:04:58 +0530

    responsive menu done

--- src/component/responsive nabar/Navbar.jsx
+++ src/component/responsive nabar/Navbar.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React ,{useState}from 'react';
 import './navbar.css';
-import { FaFacebook } from "react-icons/fa";
-import { FaInstagramSquare } from "react-icons/fa";
-import { FaTelegram } from "react-icons/fa";
+import { FaInstagramSquare,  FaFacebook, FaTelegram  } from "react-icons/fa";
+import { IoMenu } from "react-icons/io5";
+// import { useActionData, useSearchParams } from 'react-router-dom';
 
 const Navbar = () => {
+    const [showicon,seticon]=useState(false);
   return (
     <>
       <nav className='main-nav'>
@@ -13,7 +14,7 @@ const Navbar = () => {
             <span>G</span>upta
             </h2>
         </div>
-        <div className='menu-link'>
+        <div className={showicon ? "menu-link mobile-menu-link":"menu-link"}>
             <ul>
                 <li>
                     <a href='#'>Home</a>
@@ -33,13 +34,35 @@ const Navbar = () => {
         <div className='social-media'>
             <ul className='social-desktop'>
                 <li>
-                    <a href='/www.youtube.com/watch?v=8AJ3Kcz5FsM&list=PLwGdqUZWnOp3aROg4wypcRhZqJG3ajZWJ&index=87' target='_blank'><FaFacebook/></a>
-                    <a href='/www.youtube.com/watch?v=8AJ3Kcz5FsM&list=PLwGdqUZWnOp3aROg4wypcRhZqJG3ajZWJ&index=87' target='_blank'><FaInstagramSquare/></a>
-                    <a href='/www.youtube.com/watch?v=8AJ3Kcz5FsM&list=PLwGdqUZWnOp3aROg4wypcRhZqJG3ajZWJ&index=87' target='_blank'><FaTelegram/></a>
+                    <a href='/www.youtube.com/watch?v=8AJ3Kcz5FsM&list=PLwGdqUZWnOp3aROg4wypcRhZqJG3ajZWJ&index=87' target='_blank'><FaFacebook className="facebook"/></a>
                 </li>
+
+                <li>
+                    <a href='/www.youtube.com/watch?v=8AJ3Kcz5FsM&list=PLwGdqUZWnOp3aROg4wypcRhZqJG3ajZWJ&index=87' target='_blank'><FaInstagramSquare className="instagram"/></a>
+                </li>
+                <li>
+                    <a href='/www.youtube.com/watch?v=8AJ3Kcz5FsM&list=PLwGdqUZWnOp3aROg4wypcRhZqJG3ajZWJ&index=87' target='_blank'><FaTelegram className="telegram"/></a>
+                </li>
+                    
             </ul>
+            {/* hamberger menu */}
+            <div className='hamburger-menu'>
+                <a href='#' onClick={()=>{seticon(!showicon)}}><IoMenu/></a>
+            </div>
         </div>
       </nav>
+      
+      {/* hero section */}
+      <section >
+      <div className='hero-section'>
+      <div>
+      <h3>welcom to </h3>
+      <h2>developer didi page</h2>
+      </div>
+      
+      </div>
+      </section>
+       
     </>
   )
 }
